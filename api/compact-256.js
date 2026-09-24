16f0ec6a235dc9d31fb05229d87649cac4f851c5
Aathi06 <dumbcoder06@gmail.com>
2026-09-24 20:12:35 +0530

    fix routing : pass path via query param

--- api/compact-256.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Vercel function: always serves the "stack-256" page.
-const { serve } = require("../handler");
-module.exports = (req, res) => serve(req, res, "stack-256");
--- api/compact.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Vercel function: always serves the "stack-truecolor" page.
-const { serve } = require("../handler");
-module.exports = (req, res) => serve(req, res, "stack-truecolor");
