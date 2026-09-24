174f647e14e391b532886a7b657730b2b19a950f
Aathi06 <dumbcoder06@gmail.com>
2026-09-24 21:57:06 +0530

    test route

--- handler.js
+++ handler.js
@@ -46,7 +46,7 @@ function methodAllowed(req, res) {
 
 function notFound(req, res) {
   if (!methodAllowed(req, res)) return;
-  send(req, res, 404, `Not found. Try:\n  curl ${hostOf(req)}\n`, { "Cache-Control": "no-store" });
+  send(req, res, 404, `Not found. Try:\n  curl ${hostOf(req)}\n ${req.url}  ${req.path} ${req.originalUrl}`, { "Cache-Control": "no-store" });
 }
 
 function health(req, res) {
