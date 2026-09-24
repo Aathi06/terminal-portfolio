6da7b48c3b335120a0a576d5e0dfec35b554d488
Aathi06 <dumbcoder06@gmail.com>
2026-09-24 22:14:54 +0530

    test with route change

--- api/route.js
+++ api/route.js
@@ -3,6 +3,10 @@
 const { handle } = require("../handler");
 
 module.exports = (req, res) => {
-  req.url = req.query.path || "/";
-  return handle(req, res);
+  const debug = JSON.stringify(req.query);
+
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  return res.end(
+    `URL: ${req.url}\nQUERY: ${debug}\nPATH: ${req.query?.path}\n`
+  );
 };
